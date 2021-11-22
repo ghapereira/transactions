b713aa79d5971604f98d1d97c0bcb631fccd1eb7
Gustavo Pereira <ghapereira@gmail.com>
2021-11-22 19:06:07 -0300

    First commit

---
+++ src/test/java/org/ghapereira/NativeTransactionIT.java
@@ -0,0 +1,9 @@
+package org.ghapereira;
+
+import io.quarkus.test.junit.NativeImageTest;
+
+@NativeImageTest
+public class NativeTransactionIT extends TransactionTest {
+
+    // Execute the same tests but in native mode.
+}
